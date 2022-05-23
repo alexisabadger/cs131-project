3291ba8e25994c6dc8b22985fbc4acb8fe3891ec
alex (is a badger, an IRL badger) <73854333+alexisabadger@users.noreply.github.com>
2022-05-23 00:58:56 -0400

    Update reKeyThisAPI.js
    Updated documentation.

--- reKeyThisAPI.js
+++ reKeyThisAPI.js
@@ -1,32 +1,53 @@
+// MCINTIRE
+// CS 131 - Discrete Structures
+// Project: Benjamin Moore API
+
+// In this project, I take the Benjamin Moore Paints API, take it apart,
+// and re-key the nested objects so that when you're searching for a color,
+// it's able to go from O(n) in the original API to O(log n) in the new API.
+// (I then use binary search with a static index variable to show how much more
+// efficient the new color lookup actually is.) E.g. ~ 8 searches to find 'EFEEE5'('Dove White')
+
+// I chose each paint color's hex code to use as they key because it's
+// guaranteed to be both a.) unique, and b.) descriptive of the color it represents.
+
+////////////////////////////////////////////////////////////////////////////////////
+
 // required to write new API to a JSON file
 const FileSystem = require('fs');
 
-// require node-fetch (make sure to install it: npm install node-fetch)
-// const fetch = require('node-fetch');
-
 // import API that needs to be re-keyed
 const BMcolors = require('./BMcolors.json');
-
 
 // test - API is importing correctly
 // console.log(BMcolors);
 // console.log(typeof(BMcolors));
 
-// get 1st level of property names
+// test - get 1st level of property names
 // console.log(Object.getOwnPropertyNames(BMcolors));
 //for (key in BMcolors) {
     //key = BMcolors[key].hex;
   //  console.log(`${key}: ${BMcolors[key].name} - ${BMcolors[key].hex}`);
 //}
+
+// create new object (destination object)
 var newBMcolors = {};
 
+// loop through each key in the original object to add to new object
+    // destination: newBMcolors
+    // key: each original object's hex value
+    // value: each original object's original value (same as before)
 Object.keys(BMcolors).forEach(el => {
+    // tests
     // console.log(el, BMcolors[el]);
     // console.log(BMcolors[el].hex);
     Object.assign(newBMcolors, {[BMcolors[el].hex]: BMcolors[el]});
   });
 
+// create a JSON object with stringify
 const colorData = JSON.stringify(newBMcolors);
+
+// write the new updated API (JSON object) to a file
 FileSystem.writeFile('newBMcolorAPI.json', colorData, (err) => {
     if (err) {
         console.log(err);
